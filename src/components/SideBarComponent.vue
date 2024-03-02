a1f4935959c6b0348a1436a855a03b2e05526457
AllanGBOliveira <allebeira@gmail.com>
2024-03-02 12:30:25 -0300

    feat(sideMenu): side menu desktop finished

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -1,26 +1,19 @@
 <script lang="ts" setup>
-import { RouterLink } from "vue-router";
-
-import styles from "@/assets/styles/scss/components/sideBarComponent.module.scss"
+import styles from "@/assets/styles/scss/components/sideMenuComponent.module.scss"
 </script>
 
 <template>
     <div :class="styles['sideMenu-container']">
-        <RouterLink :class="styles['sideMenu-logo']" to="/">
-            <img src="@/assets/icons/logo.svg" alt="Logo" width="42" height="24" loading="eager">
-        </RouterLink>
-        <ul :class="styles['sideMenu-list']">
-            <li :class="styles['sideMenu-item']">
-                <button :class="styles['sideMenu-link']">
-                    <img src="@/assets/icons/PHONE.png" alt="Phone Icon" width="17" height="18" loading="eager">
-                </button>
-            </li>
-            <li :class="styles['sideMenu-item']">
-                <button :class="styles['sideMenu-link']">
-                    <img src="@/assets/icons/whatsapp.png" alt="WhatsApp Icon" width="17" height="17" loading="eager">
-                </button>
-            </li>
-        </ul>
+        <p :class="styles['sideMenu-title']">
+            {{ $t("message.titles.omni") }}
+        </p>
+
+
+        <form :class="styles['sideMenu-search']" novalidate>
+            <label for="search-input">
+                <font-awesome-icon :icon="['fas', 'phone']" />
+            </label>
+        </form>
+
     </div>
 </template>
-
