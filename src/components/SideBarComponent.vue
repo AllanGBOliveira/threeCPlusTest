bcad37ce21f849fbccf3d51dfed7d787c90cef85
AllanGBOliveira <allebeira@gmail.com>
2024-03-03 12:20:30 -0300

    feat(search-bar): Search Bar added

--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -1,19 +1,30 @@
 <script lang="ts" setup>
-import styles from "@/assets/styles/scss/components/sideMenuComponent.module.scss"
+import styles from "@/assets/styles/scss/components/sideBarComponent.module.scss"
 </script>
 
 <template>
-    <div :class="styles['sideMenu-container']">
-        <p :class="styles['sideMenu-title']">
-            {{ $t("message.titles.omni") }}
-        </p>
+    <div :class="styles['sideBar-container']">
+        <div :class="styles['sideBar-container-head']">
+            <p :class="styles['sideBar-title']">
+                {{ $t("message.titles.omni") }}
+            </p>
 
+            <div :class="styles['sideBar-search-container']">
+                <form :class="styles['sideBar-search']" novalidate>
+                    <label for="sideBar-search-input">
+                        <button :class="styles['sideBar-search-btn']">
+                            <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+                        </button>
+                        <input :class="styles['sideBar-search-input']" type="text" id="sideBar-search-input"
+                            :placeholder="$t('message.placeholder.search')">
+                    </label>
+                </form>
 
-        <form :class="styles['sideMenu-search']" novalidate>
-            <label for="search-input">
-                <font-awesome-icon :icon="['fas', 'phone']" />
-            </label>
-        </form>
+                <button :class="styles['sideBar-search-fab']">
+                    <font-awesome-icon :icon="['fas', 'plus']" />
+                </button>
+            </div>
 
+        </div>
     </div>
 </template>
